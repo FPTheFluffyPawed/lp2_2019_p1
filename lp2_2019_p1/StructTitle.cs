aa90f4d5abc39553920f624972b2f974854d2745
FPTheFluffyPawed <n.ito10@hotmail.com>
2019-12-09 12:41:38 +0000

    Add Struct to contain information for collection

---
+++ lp2_2019_p1/StructTitle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lp2_2019_p1
+{
+    public struct StructTitle
+    {
+        // Type of title.
+        public string TitleType { get; }
+        public string PrimaryTitle { get; }
+        public bool ForAdults { get; }
+        public short? StartYear { get; }
+        public short? EndYear { get; }
+        public IEnumerable<string> Genres { get; }
+
+        public StructTitle(
+            string titleType, string primaryTitle, bool forAdults,
+            short? startYear, short? endYear, IEnumerable<string> genres)
+        {
+            TitleType = titleType;
+            PrimaryTitle = primaryTitle;
+            ForAdults = forAdults;
+            StartYear = startYear;
+            EndYear = endYear;
+            Genres = genres;
+        }
+    }
+}
