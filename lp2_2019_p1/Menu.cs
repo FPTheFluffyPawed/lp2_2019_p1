80c4c1250ffb5f3cf823efc499d02474513d1883
FPTheFluffyPawed <n.ito10@hotmail.com>
2019-12-12 22:46:05 +0000

    Made Menu so that it can test Searcher filters

--- lp2_2019_p1/Menu.cs
+++ lp2_2019_p1/Menu.cs
@@ -6,21 +6,20 @@ namespace lp2_2019_p1
 {
     class Menu
     {
-        FileManager file = new FileManager();
         Searcher searcher = new Searcher();
-
-        string hello = "hi";
 
         public Menu()
         {
+            Console.WriteLine("Loading");
             Execute();
-            Console.WriteLine(hello);
         }
 
         private void Execute()
         {
-            Console.Write("Test");
-            hello = "hello";
+            searcher.NameInputFilter();
+            searcher.StartYearInputFilter();
+            searcher.Filter();
+            searcher.ShowResults();
         }
     }
 }
