888d402f5f91005fdf2d512efb8956707384f07a
FPTheFluffyPawed <n.ito10@hotmail.com>
2019-12-12 20:26:59 +0000

    Add comments

--- lp2_2019_p1/StructTitle.cs
+++ lp2_2019_p1/StructTitle.cs
@@ -8,10 +8,20 @@ namespace lp2_2019_p1
     {
         // Type of title.
         public string TitleType { get; }
+
+        // Primary name of the title.
         public string PrimaryTitle { get; }
+
+        // If its for adults or not.
         public bool ForAdults { get; }
+
+        // The start year (if it exists).
         public short? StartYear { get; }
+
+        // The end year (if it exists).
         public short? EndYear { get; }
+
+        // Title's genres.
         public IEnumerable<string> Genres { get; }
 
         public StructTitle(
