e06df0864811544a7fe013fcf8e95f53c6c37cee
Joao Dias <filipevillar7@hotmail.com>
2019-12-12 19:49:40 +0000

    ADDs Searcher name filter

--- lp2_2019_p1/Searcher.cs
+++ lp2_2019_p1/Searcher.cs
@@ -1,10 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace lp2_2019_p1
 {
     class Searcher
     {
+
+        int numTitlesShown = 0;
+        int numTitlesToShowOnScreen = 10;
+
+        StructTitle[] queryResults;
+
+        private IEnumerable<StructTitle> titles;
+
+        public void NameFilter(string name)
+        {
+            queryResults =
+                 (from title in titles
+                  where title.PrimaryTitle.ToLower().Contains(name)
+                  select title)
+                 .OrderBy(title => title.StartYear)
+                 .ThenBy(title => title.PrimaryTitle)
+                 .ToArray();
+
+            // Mostrar os títulos, 10 de cada vez
+            while (numTitlesShown < queryResults.Length)
+            {
+                Console.WriteLine(
+                    $"\t=> Press key to see next {numTitlesToShowOnScreen} titles...");
+                Console.ReadKey(true);
+
+                // Mostrar próximos 10
+                for (int i = numTitlesShown;
+                    i < numTitlesShown + numTitlesToShowOnScreen
+                        && i < queryResults.Length;
+                    i++)
+                {
+                    // Usar para melhorar a forma como mostramos os géneros
+                    bool firstGenre = true;
+
+                    // Obter titulo atual
+                    StructTitle title = queryResults[i];
+
+                    // Mostrar informação sobre o título
+                    Console.Write("\t\t* ");
+                    Console.Write($"\"{title.PrimaryTitle}\" ");
+                    Console.Write($"({title.StartYear?.ToString() ?? "unknown year"}): ");
+                    foreach (string genre in title.Genres)
+                    {
+                        if (!firstGenre) Console.Write("/ ");
+                        Console.Write($"{genre} ");
+                        firstGenre = false;
+                    }
+                    Console.WriteLine();
+                }
+
+                // Próximos 10
+                numTitlesShown += numTitlesToShowOnScreen;
+            }
+        }
     }
+        
 }
