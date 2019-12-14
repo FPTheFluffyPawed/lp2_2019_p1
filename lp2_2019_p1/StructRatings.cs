b31f809d0e8062c63040e66860b174d290ccef90
FPTheFluffyPawed <n.ito10@hotmail.com>
2019-12-14 12:44:22 +0000

    Setup the class so we can store ratings

---
+++ lp2_2019_p1/StructRatings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lp2_2019_p1
+{
+    public struct StructRatings
+    {
+        // Unique identifier.
+        public string RatingsIdentifier { get; }
+
+        // Rating given to the title.
+        public string RatingsAverage { get; }
+
+        public StructRatings(string ratingsIdentifier, string ratingsAverage)
+        {
+            RatingsIdentifier = ratingsIdentifier;
+            RatingsAverage = ratingsAverage;
+        }
+    }
+}
