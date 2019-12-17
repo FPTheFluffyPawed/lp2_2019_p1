a37d9f08914a3dacd41276e0e491de440b33ae57
diogo-h <diogo.m.henriques98@gmail.com>
2019-12-17 14:17:10 +0000

    Remove Menu Choose by people option

--- lp2_2019_p1/Menu.cs
+++ lp2_2019_p1/Menu.cs
@@ -21,17 +21,12 @@ namespace lp2_2019_p1
 
         private void Execute()
         {
-            menus.Add("menu1", "1 - Choose by title \n2 - Choose by people");
+            menus.Add("menu1", "1 - Choose by title");
 
             // titles path (decision 1)
             menus.Add("menu2", "1 - Filter type \n2 - Show results \nb - Back");
             menus.Add("menu3", "1 -  \nb - Back");
             menus.Add("menu4", "b - Back");
-
-            // people path (decision 2)
-            menus.Add("menu5", "1 - Type the name of the person \nb - Back");
-            menus.Add("menu6", "1 - Show more details \nb - Back");
-            menus.Add("menu7", "b - Back");
 
             currentMenu = "menu1";
             while (true)
